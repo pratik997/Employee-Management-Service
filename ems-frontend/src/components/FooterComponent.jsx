35fee1fcf0ce8d8a8cce1921366957464104cc58
pratik997 <Firstjob@072022>
2024-07-10 06:45:39 +0530

    Adding Frontend for Employee Management Service

---
+++ ems-frontend/src/components/FooterComponent.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+
+const FooterComponent = () => {
+  return (
+    <div>
+        <footer className='footer'>
+            <span>All rights reserved 2024 by PP</span>
+        </footer>
+    </div>
+  )
+}
+
+export default FooterComponent
---
+++ ems-frontend/src/components/HeaderComponent.jsx
@@ -0,0 +1,15 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+
+const HeaderComponent = () => {
+  return (
+    <div>
+        <header>
+            <nav className='navbar navbar-dark bg-dark'>
+                <a className='navbar-brand' href="http://localhost:3000/">Employee Management System</a>
+            </nav>
+        </header>
+    </div>
+  )
+}
+export default HeaderComponent
